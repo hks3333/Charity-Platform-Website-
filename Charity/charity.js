d1a2f2a32e19416f24cf6299bce73ef29dcafe41
hks3333 <116485370+hks3333@users.noreply.github.com>
2023-05-07 14:36:38 +0530

    web

---
+++ Charity/charity.js
@@ -0,0 +1,59 @@
+var totalmoney=0;
+
+function calculate() {
+    totalmoney+=document.getElementById("money").value;
+}
+function char1() {
+    document.getElementById("overlay").style.display="block";
+    document.getElementById("main").style.filter="blur(5px)";
+    document.getElementById("overlay-h2").innerHTML="Amrita Foundation";
+    document.getElementById("overlay-p").innerHTML="At Amrita Foundation, we believe that everyone deserves a safe and secure home, access to food and clean water, and the opportunity to thrive. We are a non-profit organization committed to making a positive impact on the world by providing shelter, food, and other essentials to those in need. Our mission is to empower communities by helping them build homes that are safe, sustainable, and affordable. We believe that access to safe housing is a fundamental right, and we work tirelessly to provide families with a place to call home."
+}
+function char2() {
+    document.getElementById("overlay").style.display="block";
+    document.getElementById("main").style.filter="blur(5px)";
+    document.getElementById("overlay-h2").innerHTML="Giving Hearts Foundation";
+    document.getElementById("overlay-p").innerHTML="At Amrita Foundation, we believe that everyone deserves a safe and secure home, access to food and clean water, and the opportunity to thrive. We are a non-profit organization committed to making a positive impact on the world by providing shelter, food, and other essentials to those in need. Our mission is to empower communities by helping them build homes that are safe, sustainable, and affordable. We believe that access to safe housing is a fundamental right, and we work tirelessly to provide families with a place to call home."
+}
+function char3() {
+    document.getElementById("overlay").style.display="block";
+    document.getElementById("main").style.filter="blur(5px)";
+    document.getElementById("overlay-h2").innerHTML="Amrita Foundation";
+    document.getElementById("overlay-p").innerHTML="At Amrita Foundation, we believe that everyone deserves a safe and secure home, access to food and clean water, and the opportunity to thrive. We are a non-profit organization committed to making a positive impact on the world by providing shelter, food, and other essentials to those in need. Our mission is to empower communities by helping them build homes that are safe, sustainable, and affordable. We believe that access to safe housing is a fundamental right, and we work tirelessly to provide families with a place to call home."
+}
+function char4() {
+    document.getElementById("overlay").style.display="block";
+    document.getElementById("main").style.filter="blur(5px)";
+    document.getElementById("overlay-h2").innerHTML="Amrita Foundation";
+    document.getElementById("overlay-p").innerHTML="At Amrita Foundation, we believe that everyone deserves a safe and secure home, access to food and clean water, and the opportunity to thrive. We are a non-profit organization committed to making a positive impact on the world by providing shelter, food, and other essentials to those in need. Our mission is to empower communities by helping them build homes that are safe, sustainable, and affordable. We believe that access to safe housing is a fundamental right, and we work tirelessly to provide families with a place to call home."
+}
+function char5() {
+    document.getElementById("overlay").style.display="block";
+    document.getElementById("main").style.filter="blur(5px)";
+    document.getElementById("overlay-h2").innerHTML="Amrita Foundation";
+    document.getElementById("overlay-p").innerHTML="At Amrita Foundation, we believe that everyone deserves a safe and secure home, access to food and clean water, and the opportunity to thrive. We are a non-profit organization committed to making a positive impact on the world by providing shelter, food, and other essentials to those in need. Our mission is to empower communities by helping them build homes that are safe, sustainable, and affordable. We believe that access to safe housing is a fundamental right, and we work tirelessly to provide families with a place to call home."
+}
+function char6() {
+    document.getElementById("overlay").style.display="block";
+    document.getElementById("main").style.filter="blur(5px)";
+    document.getElementById("overlay-h2").innerHTML="Amrita Foundation";
+    document.getElementById("overlay-p").innerHTML="At Amrita Foundation, we believe that everyone deserves a safe and secure home, access to food and clean water, and the opportunity to thrive. We are a non-profit organization committed to making a positive impact on the world by providing shelter, food, and other essentials to those in need. Our mission is to empower communities by helping them build homes that are safe, sustainable, and affordable. We believe that access to safe housing is a fundamental right, and we work tirelessly to provide families with a place to call home."
+}
+function char7() {
+    document.getElementById("overlay").style.display="block";
+    document.getElementById("main").style.filter="blur(5px)";
+    document.getElementById("overlay-h2").innerHTML="Amrita Foundation";
+    document.getElementById("overlay-p").innerHTML="At Amrita Foundation, we believe that everyone deserves a safe and secure home, access to food and clean water, and the opportunity to thrive. We are a non-profit organization committed to making a positive impact on the world by providing shelter, food, and other essentials to those in need. Our mission is to empower communities by helping them build homes that are safe, sustainable, and affordable. We believe that access to safe housing is a fundamental right, and we work tirelessly to provide families with a place to call home."
+}
+function char8() {
+    document.getElementById("overlay").style.display="block";
+    document.getElementById("main").style.filter="blur(5px)";
+    document.getElementById("overlay-h2").innerHTML="Amrita Foundation";
+    document.getElementById("overlay-p").innerHTML="At Amrita Foundation, we believe that everyone deserves a safe and secure home, access to food and clean water, and the opportunity to thrive. We are a non-profit organization committed to making a positive impact on the world by providing shelter, food, and other essentials to those in need. Our mission is to empower communities by helping them build homes that are safe, sustainable, and affordable. We believe that access to safe housing is a fundamental right, and we work tirelessly to provide families with a place to call home."
+}
+
+
+function popdown() {
+    document.getElementById("overlay").style.display="none";
+    document.getElementById("main").style.filter="blur(0px)";
+}
